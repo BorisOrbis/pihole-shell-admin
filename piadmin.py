70df96d2e618aac59d103f23a3152e0e299fc7d6
BorisOrbis <50413711+BorisOrbis@users.noreply.github.com>
2019-05-12 10:53:06 +0200

    Update piadmin.py

--- piadmin.py
+++ piadmin.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
-
+#
+#  Copyright (C) OrbisBoris <orbisboris@gmail.com>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License version 3 as
+#   published by the Free Software Foundation.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
 #   Don't forget to run chmod +x piadmin.py before first use
 #   PiHole Admin is used when the web admin is not working or tu shut down RPi
 #   when starting use python3 piadmin.py or else python 2.7 will be default
