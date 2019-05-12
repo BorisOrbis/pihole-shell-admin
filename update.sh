c41bfa026c09ef5b95321d1783779427d01312da
Orbis Boris <50413711+BorisOrbis@users.noreply.github.com>
2019-05-12 13:07:57 +0200

    Add files via upload

--- update.sh
+++ update.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-cd /home/pi/pihole-shell-admin && git pull
+rm -rdf /home/pi/pihole-shell-admin
+cd /home/pi/pihole-shell-admin && git pull "https://github.com/BorisOrbis/pihole-shell-admin.git"
