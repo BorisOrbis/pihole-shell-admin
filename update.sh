72d719a348e4979a7336c62b82a99c94a33f153f
Orbis Boris <50413711+BorisOrbis@users.noreply.github.com>
2019-05-12 13:36:39 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm -rdf /home/pi/pihole-shell-admin
-cd /home/pi/pihole-shell-admin && git reset --hard &&  git pull 
+cd /home/pi/pihole-shell-admin && git reset --hard &&  git pull "https://github.com/BorisOrbis/pihole-shell-admin.git"
