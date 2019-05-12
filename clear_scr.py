49549e499f03388cfb9d034ffe518af8d0c5c4ef
BorisOrbis <50413711+BorisOrbis@users.noreply.github.com>
2019-05-12 10:47:23 +0200

    new version

---
+++ clear_scr.py
@@ -0,0 +1,52 @@
+# Functions for clearing screen of shell 'end_clear'
+# and for Y/n choosing on the end of the program
+# used in piadmin.py.
+
+import subprocess
+import men as me
+
+
+sig = str  # declare sig and pro as strings
+pro = str
+
+
+def end_clear():
+    input('Press enter to continue...')
+    subprocess.call(['clear'])
+    print(me.menu)
+
+
+def sure_reboot():
+    while True:  # Yes or no loop
+        try:
+            pro = (["Y", "n"])
+            sig = str(input('Are you sure (Y/n)'))
+        except ValueError:
+            print('Sorry, your input is not correct.')
+        if sig not in pro:
+            print('Please input Y ili n')
+        if sig == 'Y':
+            print('RPi restarting...')
+            subprocess.call(['sudo', 'reboot'])
+        if sig == 'n':
+            subprocess.call(['clear'])
+            print(me.menu)
+            break
+
+
+def sure_shutdown():
+    while True:
+        try:
+            pro = (["Y", "n"])
+            sig = str(input('Are you sure (Y/n)'))
+        except ValueError:
+            print('Sorry, your input is not correct.')
+        if sig not in pro:
+            print('Please input Y ili n')
+        if sig == 'Y':
+            print('RPi is shouting down...')
+            subprocess.call(['sudo', 'halt'])
+        if sig == 'n':
+            subprocess.call(['clear'])
+            print(me.menu)
+            break
---
+++ piadmin.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+
+#   Don't forget to run chmod +x piadmin.py before first use
+#   PiHole Admin is used when the web admin is not working or tu shut down RPi
+#   when starting use python3 piadmin.py or else python 2.7 will be default
+
+# Import subprocess for sending commands to ssh bash
+import subprocess
+import time
+import men as me
+import clear_scr as cle
+
+subprocess.call(['clear'])  # calling command clear in bash
+print(me.head)  # printing head and menu
+print(me.menu)
+
+# main loop
+while True:
+    try:
+        lista = (["1", "2", "3", "4", "5", "6", "q", "r", "x", "v", "p", "g", "d", "e", "a"])
+        op = str(input('Please enter your selection: '))
+    except ValueError:
+        print('Sorry, your input is not correct.')
+        continue
+    if op not in lista:
+        print('Sorry, your input is not correct pls. try again:')
+        continue
+    if op == '1':
+        subprocess.call(['clear'])  # clear screen in terminal
+        print('PiHole update started...')
+        subprocess.call(['pihole', '-up'])  # execute pihole -up in terminal
+        cle.end_clear()
+    if op == '2':
+        subprocess.call(['clear'])
+        print('PiHole status...')
+        subprocess.call(['pihole', 'status'])
+        cle.end_clear()
+    if op == '3':
+        subprocess.call(['clear'])
+        print('PiHole is restarting DNS...')
+        subprocess.call(['pihole', 'restartdns'])
+        cle.end_clear()
+    if op == '4':
+        subprocess.call(['clear'])
+        print('PiHole debugging...')
+        subprocess.call(['pihole', '-d'])
+        cle.end_clear()
+    if op == '5':
+        subprocess.call(['clear'])
+        print('PiHole chronometer...')
+        subprocess.call(['pihole', '-c'])
+        cle.end_clear()
+    if op == '6':
+        subprocess.call(['clear'])
+        print('Flushing log...')
+        subprocess.call(['pihole', '-f'])
+        cle.end_clear()
+    if op == 'r':
+        subprocess.call(['clear'])
+        print('RPi is going to restart.')
+        cle.sure_reboot()
+    if op == 'x':
+        subprocess.call(['clear'])
+        print('RPi is going to shut down.')
+        cle.sure_shutdown()
+    if op == 'a':
+        subprocess.call(['clear'])
+        print(me.about)
+        cle.end_clear()
+    if op == 'v':
+        subprocess.call(['clear'])
+        print('Pihole version...')
+        subprocess.call(['pihole', '-v', '-c'])
+        cle.end_clear()
+    if op == 'p':
+        subprocess.call(['clear'])
+        print('Pihole change password...')
+        pas = input('Please enter your new password: ')
+        subprocess.call(['pihole', '-a', '-p', pas])
+        cle.end_clear()
+    if op == 'g':
+        subprocess.call(['clear'])
+        print('Pihole update Gravity...')
+        subprocess.call(['pihole', 'updateGravity'])
+        cle.end_clear()
+    if op == 'e':
+        subprocess.call(['clear'])
+        print('Pihole enabling...')
+        subprocess.call(['pihole', 'enable'])
+        cle.end_clear()
+    if op == 'd':
+        subprocess.call(['clear'])
+        print('Pihole disabling...')
+        subprocess.call(['pihole', 'disable'])
+        cle.end_clear()
+    if op == 'q':
+        print('Thank you for using control panel and good bye!')
+        time.sleep(3)
+        subprocess.call(['clear'])
+        break
+# end of main loop
