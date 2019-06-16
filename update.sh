a8b571d641bb4b794dd4509a73f0c4a11bf4539a
Orbis Boris <50413711+BorisOrbis@users.noreply.github.com>
2019-06-16 16:56:17 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,17 +1,2 @@
 #!/bin/sh
-
-UPSTREAM=${1:-'@{u}'}
-LOCAL=$(git rev-parse @)
-REMOTE=$(git rev-parse "$UPSTREAM")
-BASE=$(git merge-base @ "$UPSTREAM")
-
-if [ $LOCAL = $REMOTE ]; then
-    echo "Up-to-date"
-elif [ $LOCAL = $BASE ]; then
-    echo "Need to pull"
-    git reset --hard && git pull &>/dev/null
-#elif [ $REMOTE = $BASE ]; then
-#    echo "Need to push"
-else
-    echo "Error...try again."
-fi
+cd /home/pi/pihole-shell-admin && git reset --hard && git pull "https://github.com/BorisOrbis/pihole-shell-admin.git" &>/dev/null
