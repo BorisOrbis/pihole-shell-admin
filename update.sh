929db564f7eff9ac57e8ccff4248b28f39c5068e
Orbis Boris <50413711+BorisOrbis@users.noreply.github.com>
2019-06-16 16:49:10 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,2 +1,17 @@
-#!/bin/bash
-cd /home/pi/pihole-shell-admin && git reset --hard && git pull "https://github.com/BorisOrbis/pihole-shell-admin.git" &>/dev/null
+#!/bin/sh
+
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+    echo "Need to pull"
+    cd /home/pi/pihole-shell-admin && git reset --hard && git pull "https://github.com/BorisOrbis/pihole-shell-admin.git" &>/dev/null
+#elif [ $REMOTE = $BASE ]; then
+#    echo "Need to push"
+else
+    echo "Error...try again."
+fi
